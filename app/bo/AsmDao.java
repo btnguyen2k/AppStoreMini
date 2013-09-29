95cc46c89044cd53bb073feaaddcd731d672fded
btnguyen2k <btnguyen2k@gmail.com>
2013-09-29 12:48:25 +0700

    Update

--- app/bo/AsmDao.java
+++ app/bo/AsmDao.java
@@ -10,6 +10,7 @@ public class AsmDao extends BaseMysqlDao {
 
     public final static String TABLE_PLATFORM = "asm_platform";
     public final static String TABLE_USERGROUP = "asm_usergroup";
+    public final static String TABLE_USER = "asm_user";
 
     /*----------------------------------------------------------------------*/
     private static String cacheKeyPlatform(String id) {
@@ -20,12 +21,20 @@ public class AsmDao extends BaseMysqlDao {
         return "USERGROUP_" + id;
     }
 
+    private static String cacheKeyUser(String id) {
+        return "USER_" + id;
+    }
+
     private static String cacheKey(PlatformBo platform) {
         return cacheKeyPlatform(platform.getId());
     }
 
     private static String cacheKey(UsergroupBo usergroup) {
         return cacheKeyUsergroup(usergroup.getId());
+    }
+
+    private static String cacheKey(UserBo user) {
+        return cacheKeyUser(user.getId());
     }
 
     /*----------------------------------------------------------------------*/
@@ -129,7 +138,7 @@ public class AsmDao extends BaseMysqlDao {
     public static void delete(UsergroupBo usergroup) {
         final String[] COLUMNS = new String[] { "gid" };
         final Object[] VALUES = new Object[] { usergroup.getId() };
-        delete(TABLE_PLATFORM, COLUMNS, VALUES);
+        delete(TABLE_USERGROUP, COLUMNS, VALUES);
         removeFromCache(cacheKey(usergroup));
     }
 
@@ -174,5 +183,79 @@ public class AsmDao extends BaseMysqlDao {
             putToCache(CACHE_KEY, dbRow);
         }
         return (UsergroupBo) usergroup.markClean();
+    }
+
+    /*----------------------------------------------------------------------*/
+    /**
+     * Creates a new user account.
+     * 
+     * @param user
+     * @return
+     */
+    public static UserBo create(UserBo user) {
+        final String[] COLUMNS = new String[] { "uid", "ulogin_name", "upassword", "uemail",
+                "ugroup_id", "utimestamp_create" };
+        final Object[] VALUES = new Object[] { user.getId(), user.getLoginName(),
+                user.getPassword(), user.getEmail(), user.getGroupId(), user.getTimestampCreate() };
+        insertIgnore(TABLE_USER, COLUMNS, VALUES);
+        removeFromCache(cacheKey(user));
+        return (UserBo) user.markClean();
+    }
+
+    /**
+     * Deletes an existing user account.
+     * 
+     * @param user
+     */
+    public static void delete(UserBo user) {
+        final String[] COLUMNS = new String[] { "uid" };
+        final Object[] VALUES = new Object[] { user.getId() };
+        delete(TABLE_USER, COLUMNS, VALUES);
+        removeFromCache(cacheKey(user));
+    }
+
+    /**
+     * Gets a user account by id.
+     * 
+     * @param id
+     * @param clazz
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static UserBo getUser(String id) {
+        final String CACHE_KEY = cacheKeyUser(id);
+        Map<String, Object> dbRow = getFromCache(CACHE_KEY, Map.class);
+        if (dbRow == null) {
+            final String SQL_TEMPLATE = "SELECT uid AS {1}, ulogin_name AS {2}, upassword AS {3}, uemail AS {4}, ugroup_id AS {5}, utimestamp_create AS {6} FROM {0} WHERE uid=?";
+            final String SQL = MessageFormat.format(SQL_TEMPLATE, TABLE_USER, UserBo.COL_ID,
+                    UserBo.COL_LOGIN_NAME, UserBo.COL_PASSWORD, UserBo.COL_EMAIL,
+                    UserBo.COL_GROUP_ID, UserBo.COL_TIMESTAMP_CREATE);
+            final Object[] WHERE_VALUES = new Object[] { id };
+            List<Map<String, Object>> dbResult = select(SQL, WHERE_VALUES);
+            dbRow = dbResult != null && dbResult.size() > 0 ? dbResult.get(0) : null;
+            putToCache(CACHE_KEY, dbRow);
+        }
+        return dbRow != null ? (UserBo) new UserBo().fromMap(dbRow) : null;
+    }
+
+    /**
+     * Updates an existing user account.
+     * 
+     * @param user
+     * @return
+     */
+    public static UserBo update(UserBo user) {
+        if (user.isDirty()) {
+            final String CACHE_KEY = cacheKey(user);
+            final String[] COLUMNS = new String[] { "upassword", "uemail", "ugroup_id" };
+            final Object[] VALUES = new Object[] { user.getPassword(), user.getEmail(),
+                    user.getGroupId() };
+            final String[] WHERE_COLUMNS = new String[] { "uid" };
+            final Object[] WHERE_VALUES = new Object[] { user.getId() };
+            update(TABLE_USER, COLUMNS, VALUES, WHERE_COLUMNS, WHERE_VALUES);
+            Map<String, Object> dbRow = user.toMap();
+            putToCache(CACHE_KEY, dbRow);
+        }
+        return (UserBo) user.markClean();
     }
 }
