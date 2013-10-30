757a0fee29b06a8ffb107a67c00b46f8713de67c
Thanh Nguyen <btnguyen2k@gmail.com>
2013-10-31 01:54:01 +0700

    Update: view app details

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import play.i18n.Messages;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Results;
@@ -18,9 +19,23 @@ public class Application extends Controller {
         return Results.redirect(url);
     }
 
+    /*
+     * Handles: GET:/
+     */
     public static Result index() {
         ApplicationBo[] allApps = AsmDao.getAllApplications();
         return ok(views.html.index.render(allApps));
     }
 
+    /*
+     * Handles: GET:/app/id
+     */
+    public static Result viewApp(String appId) {
+        ApplicationBo app = AsmDao.getApplication(appId);
+        if (app == null) {
+            return ok(views.html.error.render(Messages.get("error.404.app"), routes.Application
+                    .index().url()));
+        }
+        return ok(views.html.app.render(app));
+    }
 }
